8a9677364e09277a42330985172d394b0c27d1be
Evgeny Dibrovsky <105384282+EvgenyDibrovsky@users.noreply.github.com>
2023-02-14 13:08:48 +0100

    commit

--- module1/dateUtils.js
+++ module1/dateUtils.js
@@ -1,6 +1,22 @@
 function getCurentData() {
-  return Date.now();
+  return (
+    Date.now() +
+    " " +
+    "global доcтупен из любово файла" +
+    "- global(из dateUtils.js) -" +
+    global.testData
+  );
 }
+console.log(__dirname);
+console.log(__filename);
 module.exports = {
   getCurentData,
 };
+
+// npm init -y
+
+// fs.readFile(filename, [options]) - чтение файла
+// fs.writeFile(filename, data, [options]) - запись файла
+// fs.appendFile(filename, data, [options])- добавление в файл
+// fs.rename(oldPath, newPath) - переименование файла.
+// fs.unlink(path, callback) - удаление файла.
--- module1/index.js
+++ module1/index.js
@@ -1,3 +1,20 @@
-const { getCurentData } = require("./dateUtils");
-console.log(`Get curent data function result - ${getCurentData()}`);
-console.log("Я программист! )))");
+// Модуль 1. Основы Node.js
+
+// const { getCurentData } = require("./dateUtils");
+// global.testData = "global (из index.js)";
+// global.testData = 1;
+
+// console.log(`Get curent data function result - ${getCurentData()}`);
+// console.log("Я программист! )))");
+// console.log(global.testData);
+// console.log(process.env);
+// console.log(process.argv);
+// console.log(process.exit);
+
+// console.log(__dirname);
+// console.log(__filename);
+
+const Calc = require("calc-js").Calc;
+console.log(process.argv);
+const [, , a, b] = process.argv;
+console.log(new Calc(parseInt(a)).sum(parseInt(b)).finish());
